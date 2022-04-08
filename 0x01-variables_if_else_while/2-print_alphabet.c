1e29741d15751ead544c37f297caa80731f96f02
Perfect Dah <patiencedah@gmail.com>
2022-04-08 10:39:22 -0700

    prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - prints alphabeths
+ *
+ *Return: zero
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
