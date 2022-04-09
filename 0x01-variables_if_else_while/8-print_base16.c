a788821897698d362b31753363b91fa11f4cc7a7
Perfect Dah <patiencedah@gmail.com>
2022-04-08 19:33:02 -0700

    prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - prints hexedecimals
+ *
+ * Return: success zero
+ */
+
+int main(void)
+{
+	char hex
+
+	for (hex = '0x00'; hex <= '0x0f'; hex ++)
+		putchar(hex);
+	putchar('\n');
+	return (0);
+}
