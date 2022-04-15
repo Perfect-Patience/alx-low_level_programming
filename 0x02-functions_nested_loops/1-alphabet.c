ce93abc757824e63924fee308d421cee2334cb05
Perfect Dah <patiencedah@gmail.com>
2022-04-15 04:37:21 -0700

    prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - prints the lower case alphabets.
+ *
+ *Return: returns zero
+ */
+
+int main(void)
+{
+	char c = 'a'
+
+		while c <= 'z'
+		{
+			_putchar(c);
+			c++;
+		}
+	return(0);
+}
