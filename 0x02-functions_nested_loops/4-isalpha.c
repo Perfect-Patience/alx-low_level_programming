afabd568922805bf926f922bfec2f789f4cc8fbf
Perfect Dah <patiencedah@gmail.com>
2022-04-15 06:28:49 -0700

    checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- *_isalpha - checks if character is uppercase
+ *_isalpha - checks for alphabet character
  *@c: first argument
  *
- *Return: 0 if lower case and 1 if upper
+ *Return: 1 if character is an alphabeth
  */
 
 int _isalpha(int c)
 {
 	if (c <= 90 && c >= 65)
+		return (1);
+	if (c <= 122 && c >= 97)
 		return (1);
 	return (0);
 }
