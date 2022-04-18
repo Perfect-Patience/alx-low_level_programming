6bf2d715fe49c11c7a78cbc6644413e4e798b1bb
Perfect Dah <patiencedah@gmail.com>
2022-04-18 08:40:51 -0700

    prints last digit of a munber

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *print_last_digit - prints last digit of numbers
+ *@n: parameter of function
+ *
+ *Return: returns last digit.
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
