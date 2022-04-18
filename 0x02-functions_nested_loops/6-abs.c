b2842a99e04aef49abccc33550f0533dafdf3440
Perfect Dah <patiencedah@gmail.com>
2022-04-18 02:58:13 -0700

    finds the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - prints the absolute value of a number
+ *@c: argumetn of the function
+ *
+ * Return: returns 0 if successful
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+		_putchar(char(c));
+	if (c < 0)
+	{
+		c = (c + 2 * c);
+		_putchar(char(c));
+	}
+	return (0);
