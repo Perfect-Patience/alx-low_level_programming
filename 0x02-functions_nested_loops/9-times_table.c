71d995a6688b2ce1ba5152897dcb48af91aebaa2
Perfect Dah <patiencedah@gmail.com>
2022-04-19 14:44:42 -0700

    prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,16 @@
 void times_table(void)
 {
 	int n1, n2, mul;
+	char answer;
 
 	for (n1 = 0; n1 <= 9; n1++)
 	{
 		for (n2 = 0; n2 <= 9; n2++)
 		{
 			mul = n1 * n2;
-			_putchar('0' + mul);
+			answer = char(mul);
+
+			_putchar(answer);
 			if (n2 != 9)
 				_putchar(' ' + ',');
 			else
