9ec176e00b3d62376768075bdd46842744f3b007
Perfect Dah <patiencedah@gmail.com>
2022-04-20 16:57:35 -0700

    takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - resets value pointed to.
+ *@n: pointer as argument of the function
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifdef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+void reset_to_98(int *n);
+#endif
