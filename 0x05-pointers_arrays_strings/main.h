a03a14ed8276edde26fb13a1e0f17095d589e643
Perfect Dah <patiencedah@gmail.com>
2022-04-20 18:02:05 -0700

    adds _putchar function declaration to header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
 #ifdef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-int _putchar(char c);
 #endif
