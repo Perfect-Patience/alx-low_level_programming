2e418e7861f6e774aeea29a0e68cc5a470452802
Perfect Dah <patiencedah@gmail.com>
2022-04-23 01:09:56 -0700

    creates a new header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char);
@@ -6,6 +6,5 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-void puts2(char *str);
 
 #endif
