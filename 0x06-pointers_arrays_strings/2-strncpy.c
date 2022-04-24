054caba56fb8e82ef9cfbe8914aa73c4e3672a09
Perfect Dah <patiencedah@gmail.com>
2022-04-24 06:05:14 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strncpy - copys a string into another.
+ *@dest: parameter1- destination of copied string.
+ *@src: parameter2- source of string to be copied.
+ *@n: parameter3- length of srting to copy
+ *
+ *Return: returns a string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
