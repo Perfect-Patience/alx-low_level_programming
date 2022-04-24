6e86c5e2dd447f313f523287b37453e869c8a4c5
Perfect Dah <patiencedah@gmail.com>
2022-04-24 09:27:37 -0700

    reverses an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *reverse_array - reverses an array
+ *@a: parameter1- name of array to be reversed
+ *@n: parameter2- length of array.
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i = 0;
+
+	for (i = 0; i <= n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i];
+		a[n - i] = temp;
+	}
+}
