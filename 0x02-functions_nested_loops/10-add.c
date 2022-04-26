8cf455d45baad7e0cb338f422896c70e13392aaf
Perfect Dah <patiencedah@gmail.com>
2022-04-26 16:15:05 -0700

    adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *add - adds two integer
+ *@a: parameter 1
+ *@b: parameter 2
+ *
+ *Return: returns sum of two intergers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
