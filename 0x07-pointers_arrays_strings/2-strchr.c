89b3027ee29b55d66bcbce87b1e899612b6a1f92
Perfect Dah <patiencedah@gmail.com>
2022-04-26 22:47:58 -0700

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strchr - locates a character in a string
+ *
+ *@c: parameter 2, type char
+ *@s: parameter 1. type char, pointer
+ *
+ *Return: if found returns pointer else returns null.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s[i]);
+		i++;
+	}
+	return ('\0');
