1b8a7070963b2a9575c77929d67e85ef1acdccba
Perfect Dah <patiencedah@gmail.com>
2022-05-14 03:40:57 -0700

    tests if function positve_or_negative() gives correct output when given a case of 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *main - tests the function that tests if a value is negative or positive
+ *with vaalue 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_negative(i);
+	return (0);
+}
