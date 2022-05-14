f4a01fd6bb0aaedeca2118e01d5c878481e255f3
Perfect Dah <patiencedah@gmail.com>
2022-05-14 16:10:22 -0700

    finds factorial using recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *factorial - finds factorial of a number
+ *@n: integer
+ *
+ * Return: -1 if n is negative else returns factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
