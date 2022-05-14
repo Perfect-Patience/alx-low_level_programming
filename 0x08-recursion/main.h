818593a2d0081b7888d6b4505a56e6e630524bfc
Perfect Dah <patiencedah@gmail.com>
2022-05-14 16:30:51 -0700

    finds x rasied to the power y using recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,4 +5,6 @@ int _putchar(int n);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
 #endif
