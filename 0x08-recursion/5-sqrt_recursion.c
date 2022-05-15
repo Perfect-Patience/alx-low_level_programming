414fbc3c3d6ec85efe2998ac25e41f719551d8cf
Perfect Dah <patiencedah@gmail.com>
2022-05-14 17:47:42 -0700

    finds square Root using recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *find_square_root - returns natural square root of n
+ *@n: integer
+ *@i: initial guess
+ *
+ * Return: returns natural square root else -1
+ */
+int find_square_root(int n, int i)
+{
+	if (i % (n / i) == 0)
+	{
+		if (i * (n / i) == n)
+			return (i);
+		else
+			return (-1);
+		return (find_square_root(n, i++));
+	}
+}
+/**
+ *_sqrt_recursion - finds the square Root of input
+ *@n: integer
+ *
+ *Return: -1 if less than 0, 0 or 1 if either, otherwise square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n > 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (find_square_root(n, 2));
+}
