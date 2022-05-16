37b67dfaee045c7f961eb6460594fb9d30556288
Perfect Dah <patiencedah@gmail.com>
2022-05-16 06:09:59 -0700

    adds arguments to the program

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - adds arguments to the program
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ *Return: 1 if arguments contain a non-digit symbols, otherwise 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	if (argc == 1)
+		sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		j = atoi(argv[i]);
+
+		if (j == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += j;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
