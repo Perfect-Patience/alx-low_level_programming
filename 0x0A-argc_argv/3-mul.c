aa18461940b55d9705d71aa048dc18fc06d6a223
Perfect Dah <patiencedah@gmail.com>
2022-05-16 05:41:53 -0700

    prints the result of multiplying 2 program arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - multiples argumnets to main function
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ * Return: 1 if argc is not 3 otherwise 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
