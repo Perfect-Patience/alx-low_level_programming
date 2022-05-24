a4024b1ade629f05aee4597e487538e18f8ac419
Perfect Dah <patiencedah@gmail.com>
2022-05-24 16:10:12 -0700

    checks malloc return value

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,5 +20,8 @@ char *create_array(unsigned int size, char c)
 	ptr = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
+	if (ptr == NULL)
+		_puchar('E');
+	free(ptr);
 	return (ptr);
 }
