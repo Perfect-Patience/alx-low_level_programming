2a68d046dbe3c0584e4f6e2a6d037f499de1e336
Perfect Dah <patiencedah@gmail.com>
2022-06-06 12:23:43 -0700

    ptints numbers from 0-9, excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - prints numbers from 0-9, excluding 2,4
+ *
+ */
+
+void print_most_numbers(void)
+{
+	char i = 48;
+
+	for (; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+			i++;
+	
+		_putchar(i);
+	}
+	_putchar('\n');
+}
