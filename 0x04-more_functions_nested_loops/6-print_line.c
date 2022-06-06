5c6a8daad1f486e65f8dd9acaf77a0a438b21edc
Perfect Dah <patiencedah@gmail.com>
2022-06-06 14:37:37 -0700

    prints a line to the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_line - prints a line to the terminal
+ *@n: length of line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
