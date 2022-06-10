ecb3f33b0ab518babd26a8a06309258695f2f765
Perfect Dah <patiencedah@gmail.com>
2022-06-10 13:02:53 -0700

    concatenates 2 strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,9 +5,8 @@
  *string_nconcat - concatenates 2 string to a specified length
  *@s1 : string 1
  *@s2 : string 2
- *@n : length to concatenate.
- *
- *Return : pointer to concatenated string, otherwise NULL.
+ *@n : length to concatenate
+ *Return: pointer to concatenated string, otherwise NULL.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -19,6 +18,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[j])
 		j++;
 
+	if (s1 == NULL)
+		number = j;
 	if (s2 == NULL)
 		number = i;
 	else if (n >= j)
