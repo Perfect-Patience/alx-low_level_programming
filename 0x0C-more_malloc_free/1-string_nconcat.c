3c4c5d58c6bfe8046c52bacedb7d9710d1609d33
Perfect Dah <patiencedah@gmail.com>
2022-06-11 02:48:27 -0700

    concatenates 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,32 +2,36 @@
 #include <stdlib.h>
 
 /**
- *string_nconcat - concatenates 2 string to a specified length
- *@s1 : string 1
- *@s2 : string 2
- *@n : length to concatenate
- *Return: pointer to concatenated string, otherwise NULL.
+ *string_nconcat - concanteneates 2 strings
+ *@s1: string 1
+ *@s2: string 2
+ *@n: length of s2 to join to s1
+ *
+ * Return: pointer to concatenated string, or null
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int i = 0, j = 0, length;
+	unsigned int i = 0, j = 0, length = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[i])
 		i++;
 	while (s2[j])
 		j++;
 
-	if (n <= j)
+	if (n < j)
 		j = n;
 
 	length = i + j;
 
 	p = malloc(length + 1);
-
 	if (p == NULL)
-		return ('\0');
-
+		return (NULL);
 	i = 0;
 	while (s1[i])
 	{
@@ -36,11 +40,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	j = 0;
-	for (; i < length ; i++)
+	for (; i < length; i++)
 	{
 		p[i] = s2[j];
 		j++;
 	}
-	p[length + 1] = '\0';
+	p[length++] = '\0';
 	return (p);
 }
