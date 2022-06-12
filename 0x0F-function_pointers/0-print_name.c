f9471b8b9cb3659a5c3d6eb673823929ac6adde2
Perfect Dah <patiencedah@gmail.com>
2022-06-12 10:39:56 -0700

    adds documentation

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
 /**
- *
- *
- *
+ *print_name - prints name
+ *@name: pointer to name.
+ *@f: pointer to function that prints the name.
  */
 
 void print_name(char *name, void (*f)(char *))
