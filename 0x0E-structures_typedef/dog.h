c3641293be9306a6eb0bdb33555abf919311ad93
Perfect Dah <patiencedah@gmail.com>
2022-06-12 02:49:54 -0700

    creates a structure dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - creates a structure dog
+ *@name: points to the name of the dog
+ *@age: stores age
+ *@owner: points the name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
