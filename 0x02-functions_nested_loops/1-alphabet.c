ac6f3674173ed4815c27f8b9ad96157f103dd243
Dah Perfect Patience <patiencedah@gmail.com>
2023-02-21 18:24:39 -0800

    prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_alphabet - prints lowercase letters
+ *
+ */
+
+void print_alphabet(void)
+{
+	c = 'a';
+	while c <= 'z'
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
