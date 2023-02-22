a2a3d32d34d3e7e299539af83ce8024bbb6464e2
Dah Perfect Patience <patiencedah@gmail.com>
2023-02-21 18:56:45 -0800

    prints alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_alphabet_x1 - prints the lowercase letters 10 times
+ *
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+	char c;
+
+	for (i = 1; i < 11; i++)
+	{
+		while (c < 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}
