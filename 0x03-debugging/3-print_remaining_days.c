35addca3204fbbb3129e6caab81b23c0890d9f39
Dah Perfect Patience <patiencedah@gmail.com>
2023-02-23 15:54:11 -0800

    corrections

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,7 +22,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
-	else if (month == 2 && day == 6 && leap == 1)
+	else if (month >= 2 && day >= 60 && leap == 1)
 	{
 		day++;
 		printf("Day of the year: %d\n", day);
