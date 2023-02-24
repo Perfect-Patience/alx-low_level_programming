eee1de28c9cc65de2dbd9a2a7267c7bdbdea7605
Dah Perfect Patience <patiencedah@gmail.com>
2023-02-24 12:35:57 -0800

    prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_square- prints a square
+ *@size: size of square
+ */
+
+void print_square(int size)
+{
+	int j, i;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
