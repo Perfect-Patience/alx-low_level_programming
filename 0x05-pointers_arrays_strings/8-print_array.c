7ddfbae2b3354a8894535fddfc58445f9070abdd
Dah Perfect Patience <patiencedah@gmail.com>
2023-02-28 18:56:46 -0800

    prints part of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,14 +2,17 @@
 /**
  *print_array - prints a specified length of an array.
  *@a: pointer to a string
- *n: length of string to print
+ *@n: length of string to print
  */
 
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
-		printf("%d, ", a[i]);
-	printf("%d\n", a[n - 1]);
+	if ( n >= 0)
+	{
+		for (i = 0; i < n - 1; i++)
+			printf("%d, ", a[i]);
+		printf("%d\n", a[n - 1]);
+	}
 }
