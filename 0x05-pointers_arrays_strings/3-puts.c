2c1986faee74203c66550e2a6e5170d635387951
Dah Perfect Patience <patiencedah@gmail.com>
2023-02-28 16:07:15 -0800

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_puts - prints a string
+ *@str: string to print
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+		_putchar((*str + i) + 48);
+	_putchar('\n');
+}
