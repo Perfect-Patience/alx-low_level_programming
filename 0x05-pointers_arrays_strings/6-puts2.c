ad3675fb5c4cb82435090c8518ae18a29e2ba72a
Dah Perfect Patience <patiencedah@gmail.com>
2023-02-28 17:46:39 -0800

    prints every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
  *puts2 - prints every other character in a string
- *str: pointer to a string
+ *@str: pointer to a string
  */
 
 void puts2(char *str)
-{
-	int i = 0;
 
-	while (str[i] != '\0')
+
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		_putchar(*str);
+		*str += 2;
 	}
 	_putchar('\n');
 }
