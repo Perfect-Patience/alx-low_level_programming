d8e81628e276261e814b50d238138f965e608600
Dah Perfect Patience <patiencedah@gmail.com>
2023-02-28 16:51:21 -0800

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *rev_string = reverses a string
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	char c;
+	int len = 0;
+	int i, j;
+
+	while (s[len] != '\0')
+		len++;
+	j = len - 1;
+	for (i = 0; i <= len / 2; i++)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+		j--;
+	}
+}
