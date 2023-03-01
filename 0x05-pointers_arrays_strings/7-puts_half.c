f946e4bc0ab97aae2a8e01d2c979caabbe6a54fe
Dah Perfect Patience <patiencedah@gmail.com>
2023-02-28 18:43:01 -0800

    prints second half of a sentence

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,25 @@
 #include "main.h"
 /**
- *
+ *puts_half - prints second half of a string
+ *@str: pointer to string
  */
 
 void puts_half(char *str)
 {
+	int i, n, len = 0;
 
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i <= len - 1; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		n = (len - 1) / 2;
+		for (i = n + 1; i < len; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
