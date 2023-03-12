152f57d16e485cf99e7068e04d516cb80fd67959
Dah Perfect Patience <patiencedah@gmail.com>
2023-03-12 07:42:18 -0700

    capitalizes a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *cap_string - capitalizes a string
+ *@s : string
+ *
+ *Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	char sep[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
+	int j, i = 0, found = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (found == 1)
+		{
+			if (s[i + 1] > 96 && s[i + 1] < 123)
+				s[i + 1] = s[i + 1] - 32;
+			found = 0;
+		}
+		i++;
+	}
+	return (s);
+}
