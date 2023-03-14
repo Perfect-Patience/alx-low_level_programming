8986a1dc6429479f4222bb2db6c72e7b8a114608
Dah Perfect Patience <patiencedah@gmail.com>
2023-03-14 11:33:19 -0700

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strdup - copies a string into a new memory space
+ *@str : pointer to string
+ *Return: pointer to copied string
+ */
+
+char *_strdup(char *str)
+{
+	int i, len = 0;
+	char *p;
+
+	if (str == NULL)
+		return ('\0');
+	while (str[len] != '\0')
+		len++;
+	p = malloc(sizeof(*str) * len);
+	if (p == NULL)
+		return ('\0');
+	for (i = 0; i <= len; i++)
+		p[i] = str[i];
+	return (p);
+}
