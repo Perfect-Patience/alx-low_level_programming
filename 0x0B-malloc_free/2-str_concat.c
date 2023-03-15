dcc1909707354049c939a8f18a46b035fc49f723
Dah Perfect Patience <patiencedah@gmail.com>
2023-03-14 18:13:25 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,10 +12,24 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 	int len1 = 0, len2 = 0, i, j;
 
-	while (s1[len1] != '\0')
-		len1++;
-	while (s1[len2] != '\0')
-		len2++;
+	if (s1 == NULL)
+	{
+		len1 = 0;
+	}
+	else
+	{
+		while (s1[len1] != '\0')
+			len1++;
+	}
+	if (s2 == NULL)
+	{
+		len2 = 0;
+	}
+	else
+	{
+		while (s1[len2] != '\0')
+			len2++;
+	}
 
 	p = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (p == NULL)
