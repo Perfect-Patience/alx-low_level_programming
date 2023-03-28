54ee4e08033f0b8e6ca6564d9e4fe16929c7e915
Dah Perfect Patience <patiencedah@gmail.com>
2023-03-28 08:14:40 -0700

    structures

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *new_dog - creates a new dog
+ *@name : name of dog
+ *@age : age of dog
+ *@owner : owner of dog
+ *Return: pointer to new dog, otherwise NULL
+ */
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+	int len1 = 0, len2 = 0, i;
+
+	p = malloc(sizeof(*p));
+	if (p == NULL || !(owner) || !(name))
+	{
+		free(p);
+		return (NULL);
+	}
+	while (name[len1])
+		len1++;
+	while (owner[len2])
+		len2++;
+	p->name = malloc(len1);
+	p->owner = malloc(len2);
+	if (p->name == NULL || p->owner == NULL)
+	{
+		free(p->name);
+		free(p->owner);
+		free(p);
+		return (NULL);
+	}
+	p->age = age;
+	for (i = 0; i < len1; i++)
+		p->name[i] = name[i];
+	p->name[i] = '\0';
+
+	for (i = 0; i < len2; i++)
+		p->owner[i] = owner[i];
+	p->owner[i] = '\0';
+	return (p);
+}
