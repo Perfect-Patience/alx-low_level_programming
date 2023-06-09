ed898a605a4833ad0a06fdcd4d8a0b19be40aefc
Perfect Patience <patinecedah@gmail.com>
2023-06-09 16:09:42 -0700

    prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - main function
+ *@argc: number of commandline arguments
+ *@argv: array of commantline arguments
+ *Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	printf(argv[0]);
+	return (0);
+}
