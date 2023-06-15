2b4213d7148918209974c70e4afb39a897ba7022
Perfect Patience <patinecedah@gmail.com>
2023-06-15 06:05:22 -0700

    corrected

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		p[i] = malloc(sizeof(p[i]) * width);
+		p[i] = malloc(sizeof(int) * width);
 		if (p[i] == NULL)
 		{
 			for (; i >= 0; i--)
