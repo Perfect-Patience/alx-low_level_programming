c6b1f2e568486bb8723aece7ee6334128059a51a
Perfect Patience <patinecedah@gmail.com>
2023-06-15 12:25:48 -0700

    concatenates 2 strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *string_nconcat - concatenates 2 strings
+ *@s1: string 1
+ *@s2: string 2
+ *@n: length of s2 to be added to s1
+ *
+ * Return: pointer to new string , otherwise NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = 0, len2 = 0, i, j;
+	char *p;
+
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+
+	if (len2 > n)
+		len2 = n;
+
+	p = malloc(sizeof(*p) * (len1 + len2 + 1));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		p[i] = s1[i];
+	for (j = 0; j < len2; j++)
+	{
+		p[i] = s2[j];
+		i++;
+	}
+
+	return (p);
+}
