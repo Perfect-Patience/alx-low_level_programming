8b4966beece384654aa6ec763088e930ae287da0
Perfect Patience <patinecedah@gmail.com>
2023-06-15 12:33:18 -0700

    concatenates 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,10 +13,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = 0, i, j;
 	char *p;
 
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
+	if (s1 != NULL)
+	{
+		while (s1[len1])
+			len1++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[len2])
+			len2++;
+	}
 
 	if (len2 > n)
 		len2 = n;
