2491a3113523ad44058f0bddbb69b58df0c8646f
Perfect Patience <patinecedah@gmail.com>
2023-06-21 16:26:33 -0700

    prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - prints name
+ *@name: pointer to a string containing name
+ *@f: pointer to the function that prints the name
+ *
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
