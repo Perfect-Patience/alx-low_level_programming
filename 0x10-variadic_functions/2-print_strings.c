e4077c748a24e3496904eec43921fe91136c8d81
Perfect Patience <patinecedah@gmail.com>
2023-06-25 17:44:36 -0700

    question 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ *print_strings - prints strings followed by a new line
+ *@separator: separator
+ *@n: number of arguments passed
+ *
+ */
+
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *string;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(args, char *);
+		if (string)
+			printf("%s", string);
+		else
+			printf("(nil)");
+		if (separator && i != n - 1)
+			printf("%S", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
