e82ba3e197a400256055ba80b9cb80f31d328b9c
Perfect Patience <patinecedah@gmail.com>
2023-06-30 15:11:06 -0700

    corrects to betty standard

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,8 @@
  *
  */
 
-__attribute__((constructor)) void  before_main()
+void __attribute__((constructor)) before_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
