9d6351c168f792f71cc546e41c4abf0de504caf1
Perfect Patience <patinecedah@gmail.com>
2023-06-30 15:07:53 -0700

    executes before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *before_main - executes before main
+ *
+ */
+
+__attribute__((constructor)) void  before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
