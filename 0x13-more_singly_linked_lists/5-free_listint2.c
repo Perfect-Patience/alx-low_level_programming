233f56339c3c71c2855902e22d6b169d74d943fb
Perfect Patience <patinecedah@gmail.com>
2023-07-04 16:38:27 -0700

    frees linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *free_listint2 - frees a listint_t list
+ *@head : address to pointer to first node
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next_node, *h;
+
+	if (*head != NULL)
+	{
+		h = *head;
+		while (h != NULL)
+		{
+			next_node = h->next;
+			free(h);
+			h = next_node;
+		}
+		*head = NULL;
+	}
+}
+
