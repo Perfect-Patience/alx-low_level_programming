c6f0e8091174ab7d7dde78b43c3a3ffac5ecb00d
Perfect Patience <patinecedah@gmail.com>
2023-07-04 18:37:52 -0700

    correction

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 					count++;
 					current = current->next;
 				}
-				if (current == NULL)
+				if (current == NULL || (current->next == NULL && index != 0))
 					return (-1);
 				prev = current;
 				current = prev->next;
