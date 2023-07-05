687bc859b4e90b9a8abe6bb1991b1d871a669171
Perfect-Patience <101493701+Perfect-Patience@users.noreply.github.com>
2023-07-05 16:07:44 +0000

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 					count++;
 					current = current->next;
 				}
-				if (current == NULL || (current->next == NULL && index != 0))
+				if (current == NULL)
 					return (-1);
 				prev = current;
 				current = prev->next;
