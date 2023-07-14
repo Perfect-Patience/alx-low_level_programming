7992c5da4d85bda048d530fc8783f23afe71ee54
Dah Perfect Patience <patiencedah@gmail.com>
2023-07-14 22:59:24 +0000

    converts binary to decimals

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *binary_to_unit - converts binary to decimal using left shift operator
+ *@b: pointer to string of 0s and 1s
+ *
+ *Return: value in base 10;
+ */
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0
+	int power;
+
+	for (power = 0; b[power] != '\0'; power++)
+	{
+		if (b[power] != '1' && b[power] != '0')
+			return(0);
+		result += (b[power] - '0') * (1 << power);
+	}
+	return results;
+}
