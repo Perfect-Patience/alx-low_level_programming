80f120168fe59604f7a898578f48bedec26412d4
Perfect Patience <patinecedah@gmail.com>
2023-07-19 07:42:03 -0700

    converts binary to decimals

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,18 +10,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	int length = 0;
+	int length = 0, i, pow = 0;
 
 	if (b)
 	{
-		while(b[length])
+		while (b[length])
 			length++;
 
-		for (length; length != 0; length--)
+		i = length - 1;
+		for (; i >= 0;  i--)
 		{
-			if (b[length] != '1' && b[length] != '0')
+			if (b[i] != '1' && b[i] != '0')
 				return (0);
-			result += (b[length] - '0') * (1 << (length - 1));
+			result += (b[i] - '0') * (1 << pow);
+			pow++;
 		}
 	}
 	return (result);
